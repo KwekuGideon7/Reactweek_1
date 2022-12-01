78a592563d7e0738d7ca6d906ae964d9cdeb17cb
GIDEON NYEDUA ABOAH <gideonkweku7@gmail.com>
2022-12-01 06:22:22 +0000

    thirdCommit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component{
             <div className='container'>
               <NavbarBrand href='/'>
                 Ristorante Con Fusion
-
+                  
               </NavbarBrand>
             </div>
           </Navbar>
